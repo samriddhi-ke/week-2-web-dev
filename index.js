4da74c6020c2294d1c195489a329800915000505
samriddhi-ke <samriddhikedia2210@gmail.com>
2025-06-25 21:36:49 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,6 +1,9 @@
 import React from "react";
-import ReactDOM from "react-dom";
-import App from "./app";
+import ReactDOM from "react-dom/client"; // ✅ correct import for React 18
+import App from "./App";
 import "./index.css";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const rootElement = document.getElementById("root");
+const root = ReactDOM.createRoot(rootElement);
+root.render(<App />);
+
